7bbe9761d603b63e4f3efce05b3535fff0b1a42b
irlangh <irlangithub@gmail.com>
2025-05-07 13:48:38 +0000

    NIVEL AVENTUREIRO: acrescentei o movimento do cava lo, usando os laços de repetição While e For

--- xadrez.c
+++ xadrez.c
@@ -4,9 +4,9 @@ int main() {
     //MOVIMENTO DA TORRE(
     //A Torre move-se em linha reta horizontalmente ou verticalmente.
     //Simula 5 casas para a direita usando o laço FOR)
-    int T;
+    int i;
     printf("Movimento da Torre (5 casas para a Direita):\n");
-    for (T = 1; T <= 5; T++) {
+    for (i = 1; i <= 5; i++) {
         printf("Direita\n");
     }
 
@@ -15,11 +15,11 @@ int main() {
     //MOVIMENTO DO BISPO(
     //O Bispo move-se em diagonais
     //Simula 5 casas na diagonal para cima e à direita usando o laço WHILE)
-    int B = 1;
+    int j = 1;
     printf("Movimento do Bispo (5 casas na diagonal Cima Direita):\n");
-    while (B <= 5) {
+    while (j <= 5) {
         printf("Cima Direita\n");
-        B++;
+        j++;
     }
 
     printf("\n");
@@ -27,12 +27,29 @@ int main() {
     //MOVIMENTO DA RAINHA(
     //A Rainha move-se em todas as direções
     //Simula 8 casas para a esquerda usando o laço DO-WHILE)
-    int R = 1;
+    int k = 1;
     printf("Movimento da Rainha (8 casas para a Esquerda):\n");
     do {
         printf("Esquerda\n");
-        R++;
-    } while (R <= 8);
+        k++;
+    } while (k <= 8);
+
+    printf("\n");
+
+    //MOVIMENTO DO CAVALO(
+    //O Cavalo move-se em L
+    //Simula 2 casas para cima e 1 para a direita usando o laço WHILE e FOR)
+    int l = 1;
+    printf("Movimento do Cavalo (2 casas para cima e 1 para a Direita):\n");
+    while (l--) {
+        for (int m = 1; m <= 2; m++) {
+            printf("Cima\n");
+            
+        }
+        printf("Direita\n");
+
+        printf("\n");
+    }
 
     return 0;
 }
